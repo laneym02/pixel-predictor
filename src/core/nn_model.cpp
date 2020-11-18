ce1a3c982ec13bb8b03a78ab68ad69fdede83613
laneym02 <51579430+laneym02@users.noreply.github.com>
2020-11-17 21:56:15 -0600

    Add comments

--- src/core/nn_model.cpp
+++ src/core/nn_model.cpp
@@ -21,6 +21,8 @@ NeuralNetworkModel::NeuralNetworkModel(const vector<int> &layer_sizes)
 NeuralNetworkModel::NeuralNetworkModel(const vector<int> &layer_sizes,
                                        const vector<vector<double>> &weights)
     : layer_sizes_(layer_sizes), weights_(weights) {
+  // check that all weight sizes are correct
+
   if (layer_sizes_.size() != weights_.size()) {
     Reset();
     throw std::invalid_argument("different size layer sizes and weights");
@@ -43,10 +45,12 @@ void NeuralNetworkModel::Reset() {
 }
 
 void NeuralNetworkModel::RandomizeWeights() {
+  // after each layer except for the last, general a layer of weights
   for (size_t prev_layer = 0; prev_layer < layer_sizes_.size() - 1;
        ++prev_layer) {
     vector<double> layer_weights;
 
+    // generate random weights
     for (int x = 0;
          x < layer_sizes_.at(prev_layer) * layer_sizes_.at(prev_layer + 1);
          ++x) {
@@ -69,11 +73,13 @@ NeuralNetworkModel::FeedForward(const vector<double> &input) const {
 
   vector<vector<double>> all_node_values = {input};
 
+  // for each weight layer, calculate the subsequent node values
   for (size_t weight_level = 0; weight_level < weights_.size();
        ++weight_level) {
     const vector<double> &weights = weights_.at(weight_level);
     vector<double> next_layer_values;
 
+    // generate next weights
     vector<vector<double>> next_weights;
     for (int i = 0; i < layer_sizes_.at(weight_level); ++i) {
       vector<double> node_weights;
@@ -85,15 +91,16 @@ NeuralNetworkModel::FeedForward(const vector<double> &input) const {
       next_weights.push_back(node_weights);
     }
 
-    vector<double> next_cells;
-    next_cells.reserve(layer_sizes_.at(weight_level));
+    // calculate next nodes
+    vector<double> next_nodes;
+    next_nodes.reserve(layer_sizes_.at(weight_level));
     for (int i = 0; i < layer_sizes_.at(weight_level); ++i) {
-      next_cells.push_back(ActivationFunction(std::inner_product(
+      next_nodes.push_back(ActivationFunction(std::inner_product(
           all_node_values.back().begin(), all_node_values.back().end(),
           next_weights.at(i).begin(), 0.0)));
     }
 
-    all_node_values.push_back(next_cells);
+    all_node_values.push_back(next_nodes);
   }
 
   return all_node_values;

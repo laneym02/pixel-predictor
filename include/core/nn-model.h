07f7b8a66287991e0c2d41999237f1acddbf5331
laneym02 <51579430+laneym02@users.noreply.github.com>
2020-11-17 21:32:53 -0600

    Add documentation

--- include/core/nn-model.h
+++ include/core/nn-model.h
@@ -12,15 +12,40 @@ namespace pixel_predictor {
 
 namespace neural_network {
 
+/**
+ * Represents a neural network
+ */
 class NeuralNetworkModel {
 public:
+  /**
+   * Create a network
+   */
   NeuralNetworkModel();
+
+  /**
+   * Create a neural network with specified layer sizes
+   * @param layer_sizes the vector of layer sizes
+   */
   explicit NeuralNetworkModel(const vector<int> &layer_sizes);
+
+  /**
+   * Create a neural network with specified layer sizes and weights
+   * @param layer_sizes the vector of layer sizes
+   * @param weights the vector of vectors of weights
+   */
   NeuralNetworkModel(const vector<int> &layer_sizes,
                      const vector<vector<double>> &weights);
 
+  /**
+   * Pass an input vector through the output vector
+   * @param input the input vector
+   * @return the output vector
+   */
   vector<double> Output(const vector<double> &input) const;
 
+  /**
+   * Reset the network
+   */
   void Reset();
 
 private:
@@ -28,10 +53,23 @@ private:
   vector<int> layer_sizes_ = default_layer_sizes_;
   vector<vector<double>> weights_;
 
+  /**
+   * Populate weights_ with random weights
+   */
   void RandomizeWeights();
 
+  /**
+   * Calculate the node values when feeding an input vector through the network
+   * @param input the input vector
+   * @return the vector of all node values
+   */
   vector<vector<double>> FeedForward(const vector<double> &input) const;
 
+  /**
+   * The activation function
+   * @param input the input value
+   * @return the output of the activation function
+   */
   double ActivationFunction(const double &input) const;
 };
 

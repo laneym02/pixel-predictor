c8965b841a2a0b47218549cfb5b986a518bf7ce4
laneym02 <51579430+laneym02@users.noreply.github.com>
2020-11-17 21:39:57 -0600

    Add neural network tests

---
+++ tests/nn_model_test.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Laney Moy on 11/17/2020.
+//
+
+#include <core/nn-model.h>
+
+#include <catch2/catch.hpp>
+
+using pixel_predictor::neural_network::NeuralNetworkModel;
+
+TEST_CASE("Get neural network output") {
+  NeuralNetworkModel model;
+
+  SECTION("One layer") {
+
+  }
+}

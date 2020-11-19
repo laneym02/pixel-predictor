b806c41bad4daa69df96c436b3ee1d9428a06eee
laneym02 <51579430+laneym02@users.noreply.github.com>
2020-11-19 15:00:29 -0600

    Add testing

--- tests/nn_model_test.cpp
+++ tests/nn_model_test.cpp
@@ -9,9 +9,19 @@
 using pixel_predictor::neural_network::NeuralNetworkModel;
 
 TEST_CASE("Get neural network output") {
-  NeuralNetworkModel model;
 
-  SECTION("One layer") {
+  SECTION("One layer") { NeuralNetworkModel model; }
 
+  SECTION("Three layers of different sizes") {
+    vector<int> layer_sizes({2, 3, 2});
+    vector<vector<double>> weights(
+        {{1, 0.5, 0.5, 1, 1, 0.25}, {1, 0.5, 0.25, 0.5, 1, 0.5}});
+
+    NeuralNetworkModel model(layer_sizes, weights);
+
+    vector<double> output = model.Output(vector<double>({1, 2}));
+
+    REQUIRE(output.at(0) == Approx(0.8715426836));
+    REQUIRE(output.at(1) == Approx(0.7864884761));
   }
 }
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,3 +1,5 @@
 //
 // Created by Laney Moy on 11/17/2020.
 //
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>

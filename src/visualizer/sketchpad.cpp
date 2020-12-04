0d24e5e5784022d47321760865ff5e9eabc11674
laneym02 <51579430+laneym02@users.noreply.github.com>
2020-12-04 02:18:37 -0500

    Add sketchpad methods

--- src/visualizer/sketchpad.cpp
+++ src/visualizer/sketchpad.cpp
@@ -6,6 +6,18 @@
 
 namespace pixel_predictor {
 
-namespace visualizer {}
+namespace visualizer {
+
+Sketchpad::Sketchpad(const glm::vec2 &top_left_corner,
+                     size_t num_pixels_per_side, double sketchpad_size,
+                     double brush_radius) {}
+
+void Sketchpad::Draw() const {}
+
+void Sketchpad::HandleBrush(const glm::vec2 &brush_screen_coords) {}
+
+void Sketchpad::Clear() {}
+
+}
 
 } // namespace pixel_predictor

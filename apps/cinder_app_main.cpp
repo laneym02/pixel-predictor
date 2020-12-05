75126ba9f46d50f4f6410dc778480c3791740ef6
laneym02 <51579430+laneym02@users.noreply.github.com>
2020-12-05 02:03:10 -0500

    Write cinder main

--- apps/cinder_app_main.cpp
+++ apps/cinder_app_main.cpp
@@ -1,3 +1,13 @@
 //
 // Created by Laney Moy on 11/17/2020.
 //
+#include <visualizer/pixel_predictor_app.h>
+
+using pixel_predictor::visualizer::PixelPredictorApp;
+
+void prepareSettings(PixelPredictorApp::Settings* settings) {
+  settings->setResizable(false);
+}
+
+// This line is a macro that expands into an "int main()" function.
+CINDER_APP(PixelPredictorApp, ci::app::RendererGl, prepareSettings);

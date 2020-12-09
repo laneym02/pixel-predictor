59fbfcfe619331976a1ac07603453da45a23ee7a
laneym02 <51579430+laneym02@users.noreply.github.com>
2020-12-08 22:27:34 -0500

    Remove unused train-model file

--- apps/train_model_main.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Laney Moy on 11/17/2020.
-//
-
-#include <core/neural_network.h>
-
-using pixel_predictor::neural_network::NeuralNetwork;
-
-int main(int argc, char *argv[]) { return 0; }

927095d3d1a4033a4fed7a0b0a8b217c96500ab1
laneym02 <51579430+laneym02@users.noreply.github.com>
2020-12-08 22:19:34 -0500

    Update neural network testing

--- tests/neural_network_test.cpp
+++ tests/neural_network_test.cpp
@@ -15,9 +15,9 @@ TEST_CASE("Get neural network output") {
     vector<int> layer_sizes({2, 3});
     vector<vector<double>> weights({{1, 0.5, 0.5, 1, 1, 0.25}});
 
-    NeuralNetwork model(layer_sizes, weights);
+    NeuralNetwork network(layer_sizes, weights);
 
-    vector<double> output = model.Output(vector<double>({1, 2}));
+    vector<double> output = network.Output(vector<double>({1, 2}));
 
     REQUIRE(output.at(0) == Approx(0.9525741268));
     REQUIRE(output.at(1) == Approx(0.92414182));
@@ -29,9 +29,9 @@ TEST_CASE("Get neural network output") {
     vector<vector<double>> weights(
         {{1, 0.5, 0.5, 1, 1, 0.25}, {1, 0.5, 0.25, 0.5, 1, 0.5}});
 
-    NeuralNetwork model(layer_sizes, weights);
+    NeuralNetwork network(layer_sizes, weights);
 
-    vector<double> output = model.Output(vector<double>({1, 2}));
+    vector<double> output = network.Output(vector<double>({1, 2}));
 
     REQUIRE(output.at(0) == Approx(0.8715426836));
     REQUIRE(output.at(1) == Approx(0.7864884761));
@@ -41,13 +41,50 @@ TEST_CASE("Get neural network output") {
     srand(0);
     vector<int> layer_sizes({2, 3, 4, 3});
 
-    NeuralNetwork model(layer_sizes);
+    NeuralNetwork network(layer_sizes);
 
-    vector<double> output = model.Output(vector<double>({1, 2}));
+    vector<double> output = network.Output(vector<double>({1, 2}));
 
-    REQUIRE(output.at(0) == Approx(0.7772072585));
-    REQUIRE(output.at(1) == Approx(0.7494078919));
-    REQUIRE(output.at(2) == Approx(0.9138059277));
+    REQUIRE(output.at(0) == Approx(0.2483233651));
+    REQUIRE(output.at(1) == Approx(0.2225897386));
+    REQUIRE(output.at(2) == Approx(0.9658595848));
+  }
+}
+
+TEST_CASE("Invalid arguments for training and output") {
+  SECTION("Empty vector") {
+    NeuralNetwork network;
+    REQUIRE_THROWS_AS(network.Output(vector<double>()), std::invalid_argument);
+  }
+
+  SECTION("Incorrect size") {
+    NeuralNetwork network;
+    REQUIRE_THROWS_AS(network.Output(vector<double>({1})),
+                      std::invalid_argument);
+  }
+
+  SECTION("Different number of inputs and outputs") {
+    NeuralNetwork network;
+    REQUIRE_THROWS_AS(
+        network.Train(vector<vector<double>>({{1, .5}, {0.5, .25}}),
+                      vector<vector<double>>({{1, 0, .5}})),
+        std::invalid_argument);
+  }
+
+  SECTION("Incorrect input size") {
+    NeuralNetwork network;
+    REQUIRE_THROWS_AS(
+        network.Train(vector<vector<double>>({{1, .5}, {0.5}}),
+                      vector<vector<double>>({{1, 0, .5}, {0, .2, .5}})),
+        std::invalid_argument);
+  }
+
+  SECTION("Incorrect output size") {
+    NeuralNetwork network;
+    REQUIRE_THROWS_AS(
+        network.Train(vector<vector<double>>({{1, .5}, {0.5, 0.2}}),
+                      vector<vector<double>>({{1, 0, .5}, {0, .2}})),
+        std::invalid_argument);
   }
 }
 
@@ -58,13 +95,13 @@ TEST_CASE("Train neural network") {
       vector<vector<double>> weights(
           {{1, 0.5, 0.5, 1, 1, 0.25}, {1, 0.5, 0.25, 0.5, 1, 0.5}});
 
-      NeuralNetwork model(layer_sizes, weights);
-      model.SetAlpha(1);
+      NeuralNetwork network(layer_sizes, weights);
+      network.SetAlpha(1);
 
-      model.Train(vector<vector<double>>({{1, 2}}),
-                  vector<vector<double>>({{1, 0}}), 1);
+      network.Train(vector<vector<double>>({{1, 2}}),
+                    vector<vector<double>>({{1, 0}}), 1);
 
-      vector<double> output = model.Output(vector<double>({1, 2}));
+      vector<double> output = network.Output(vector<double>({1, 2}));
 
       REQUIRE(output.at(0) == Approx(0.8739710314));
       REQUIRE(output.at(1) == Approx(0.7302340494));
@@ -75,13 +112,13 @@ TEST_CASE("Train neural network") {
       vector<vector<double>> weights(
           {{1, 0.5, 0.5, 1, 1, 0.25}, {1, 0.5, 0.25, 0.5, 1, 0.5}});
 
-      NeuralNetwork model(layer_sizes, weights);
-      model.SetAlpha(1);
+      NeuralNetwork network(layer_sizes, weights);
+      network.SetAlpha(1);
 
-      model.Train(vector<vector<double>>({{1, 2}}),
-                  vector<vector<double>>({{1, 0}}), 3);
+      network.Train(vector<vector<double>>({{1, 2}}),
+                    vector<vector<double>>({{1, 0}}), 3);
 
-      vector<double> output = model.Output(vector<double>({1, 2}));
+      vector<double> output = network.Output(vector<double>({1, 2}));
 
       REQUIRE(output.at(0) == Approx(0.8788204623));
       REQUIRE(output.at(1) == Approx(0.5814688036));
@@ -92,13 +129,13 @@ TEST_CASE("Train neural network") {
       vector<vector<double>> weights(
           {{1, 0.5, 0.5, 1, 1, 0.25}, {1, 0.5, 0.25, 0.5, 1, 0.5}});
 
-      NeuralNetwork model(layer_sizes, weights);
-      model.SetAlpha(1);
+      NeuralNetwork network(layer_sizes, weights);
+      network.SetAlpha(1);
 
-      model.Train(vector<vector<double>>({{1, 2}, {0.5, 2.5}}),
-                  vector<vector<double>>({{1, 0}, {0.5, 0.2}}), 1);
+      network.Train(vector<vector<double>>({{1, 2}, {0.5, 2.5}}),
+                    vector<vector<double>>({{1, 0}, {0.5, 0.2}}), 1);
 
-      vector<double> output = model.Output(vector<double>({1, 2}));
+      vector<double> output = network.Output(vector<double>({1, 2}));
 
       REQUIRE(output.at(0) == Approx(0.8606789496));
       REQUIRE(output.at(1) == Approx(0.6794101896));
@@ -109,13 +146,13 @@ TEST_CASE("Train neural network") {
       vector<vector<double>> weights(
           {{1, 0.5, 0.5, 1, 1, 0.25}, {1, 0.5, 0.25, 0.5, 1, 0.5}});
 
-      NeuralNetwork model(layer_sizes, weights);
-      model.SetAlpha(1);
+      NeuralNetwork network(layer_sizes, weights);
+      network.SetAlpha(1);
 
-      model.Train(vector<vector<double>>({{1, 2}, {0.5, 2.5}}),
-                  vector<vector<double>>({{1, 0}, {0.5, 0.2}}), 10);
+      network.Train(vector<vector<double>>({{1, 2}, {0.5, 2.5}}),
+                    vector<vector<double>>({{1, 0}, {0.5, 0.2}}), 10);
 
-      vector<double> output = model.Output(vector<double>({1, 2}));
+      vector<double> output = network.Output(vector<double>({1, 2}));
 
       REQUIRE(output.at(0) == Approx(0.7927611541));
       REQUIRE(output.at(1) == Approx(0.1984379229));
@@ -125,17 +162,17 @@ TEST_CASE("Train neural network") {
       srand(0);
       vector<int> layer_sizes({2, 3, 4, 3});
 
-      NeuralNetwork model(layer_sizes);
-      model.SetAlpha(1);
+      NeuralNetwork network(layer_sizes);
+      network.SetAlpha(1);
 
-      model.Train(vector<vector<double>>({{1, .5}, {0.5, .25}}),
-                  vector<vector<double>>({{1, 0, .5}, {0.5, 0.2, .7}}), 20);
+      network.Train(vector<vector<double>>({{1, .5}, {0.5, .25}}),
+                    vector<vector<double>>({{1, 0, .5}, {0.5, 0.2, .7}}), 20);
 
-      vector<double> output = model.Output(vector<double>({1, .5}));
+      vector<double> output = network.Output(vector<double>({1, .5}));
 
-      REQUIRE(output.at(0) == Approx(0.7517122526));
-      REQUIRE(output.at(1) == Approx(0.1490035724));
-      REQUIRE(output.at(2) == Approx(0.6305182434));
+      REQUIRE(output.at(0) == Approx(0.7067299923));
+      REQUIRE(output.at(1) == Approx(0.1649370727));
+      REQUIRE(output.at(2) == Approx(0.9063482244));
     }
   }
 }
